40f593d99c529eaf0262b15e22784f72764c3dbb
Faizal <faizallivai@gmail.com>
2024-04-01 15:22:52 +0700

    Commitegein

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPagePocariComponent } from './landing-page-pocari/landing-page-pocari.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Definisikan jalur langsung ke komponen landing page
+  { path: '', component: LandingPagePocariComponent },
+  // Anda juga bisa menambahkan jalur-jalur lainnya di sini jika diperlukan
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
--- src/app/landing-page-pocari/landing-page-pocari.component.ts
+++ src/app/landing-page-pocari/landing-page-pocari.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class LandingPagePocariComponent {
 
+    videoPlayer:any = document.getElementById('videoPlayer');
+
+  openmodal() {
+    var modal = document.getElementById("myModal");
+    modal!.style.display = "block";
+  }
+
+  closemodal() {
+    var modal = document.getElementById("myModal");
+    modal!.style.display = "none";
+    this.videoPlayer!.play();
+  }
+
+ span = document.getElementsByClassName("close")[0];
+
+  onclickpause () {
+    this.closemodal();
+  }
+
+  eventkeydown(event:any) {
+    if (event.ctrlKey && (event.key === 'c' || event.key === 'u' || event.key === 'i' || event.key === 'j' || event.key === 'k' || event.key === 's' || event.key === 'x' || event.key === 'q')) {
+      event.preventDefault();
+      this.openmodal();
+  }
+}
+
+  eventkeyup (event:any) {
+    this.openmodal();
+      this.videoPlayer.pause();
+    navigator.clipboard.writeText('');
+  }
+
+  resizealert () {
+    this.openmodal(); // Open modal when resizing window
+    alert('Dilarang mengambil screenshot dari halaman ini!!');
+  }
+
+  gotovideo () {
+    //Redirect to the video page
+    window.location.href = "src/assets/videos/Proses Produksi Pocari Sweat.mp4";
+  }
+
+    
 }
