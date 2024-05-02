6255320058a7a068cef41418d7007462dd4ce755
Faizal <faizallivai@gmail.com>
2024-05-02 16:49:51 +0700

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   // Definisikan jalur langsung ke komponen landing page
   { path: '', component: LandingPagePocariComponent },
   // Anda juga bisa menambahkan jalur-jalur lainnya di sini jika diperlukan
-  { path: '/src/app/admin-login-page-pocari/admin-login-page-pocari.component.html', component: AdminLoginPagePocariComponent},
-  { path: '/src/app/admin-dashboard-page-pocari/admin-dashboard-page-pocari.component.html', component: AdminDashboardPagePocariComponent}
+  { path: 'loginAdmin', component: AdminLoginPagePocariComponent},
+  { path: 'dashboardAdmin', component: AdminDashboardPagePocariComponent}
 ];
 
 @NgModule({
--- src/app/landing-page-pocari/landing-page-pocari.component.ts
+++ src/app/landing-page-pocari/landing-page-pocari.component.ts
@@ -1,58 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page-pocari',
   templateUrl: './landing-page-pocari.component.html',
   styleUrls: ['./landing-page-pocari.component.css']
 })
-export class LandingPagePocariComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-    videoPlayer:any = document.getElementById('videoPlayer');
+export class LandingPagePocariComponent {
+//   ngOnInit(): void {
+//   }
+//     videoPlayer:any = document.getElementById('videoPlayer');
 
-  openmodal() {
-    var modal = document.getElementById("myModal");
-    modal!.style.display = "block";
-  }
+//   openmodal() {
+//     var modal = document.getElementById("myModal");
+//     modal!.style.display = "block";
+//   }
 
-  closemodal() {
-    var modal = document.getElementById("myModal");
-    modal!.style.display = "none";
-    if (this.videoPlayer) {
-      this.videoPlayer!.play();
-    }
-  }
+//   closemodal() {
+//     var modal = document.getElementById("myModal");
+//     modal!.style.display = "none";
+//     if (this.videoPlayer) {
+//       this.videoPlayer!.play();
+//     }
+//   }
 
-span = document.getElementsByClassName("close")[0];
-  onclickpause () {
-    this.closemodal();
-  }
+// span = document.getElementsByClassName("close")[0];
+//   onclickpause () {
+//     this.closemodal();
+//   }
 
-  eventkeydown(event:any) {
-    if (event.ctrlKey && (event.key === 'c' || event.key === 'u' || event.key === 'i' || event.key === 'j' || event.key === 'k' || event.key === 's' || event.key === 'x' || event.key === 'q')) {
-      event.preventDefault();
-      this.openmodal();
-      alert('Dilarang mengambil screenshot dari halaman ini!!');
-  }
-}
+//   eventkeydown(event:any) {
+//     if (event.ctrlKey && (event.key === 'c' || event.key === 'u' || event.key === 'i' || event.key === 'j' || event.key === 'k' || event.key === 's' || event.key === 'x' || event.key === 'q')) {
+//       event.preventDefault();
+//       this.openmodal();
+//       alert('Dilarang mengambil screenshot dari halaman ini!!');
+//   }
+// }
 
-  eventkeyup (event:any) {
-    this.openmodal();
-    alert('Dilarang mengambil screenshot dari halaman ini!!');
-      this.videoPlayer.pause();
-    navigator.clipboard.writeText('');
-  }
+//   eventkeyup (event:any) {
+//     this.openmodal();
+//     alert('Dilarang mengambil screenshot dari halaman ini!!');
+//       this.videoPlayer.pause();
+//     navigator.clipboard.writeText('');
+//   }
 
-  resizealert () {
-    this.openmodal(); // Open modal when resizing window
-    alert('Dilarang mengambil screenshot dari halaman ini!!');
-  }
+//   resizealert () {
+//     this.openmodal(); // Open modal when resizing window
+//     alert('Dilarang mengambil screenshot dari halaman ini!!');
+//   }
 
-  gotovideo () {
-    //Redirect to the video page
-    window.location.href = "src/assets/videos/Proses_Produksi_Pocari_Sweat.mp4";
-  }
-
-    
+//   gotovideo () {
+//     //Redirect to the video page
+//     window.location.href = "src/assets/videos/Proses_Produksi_Pocari_Sweat.mp4";
+//   }    
 }
